2dfdb22839b33a08bbdfd54a1948ad631e4bdae6
Brian McNabb <brian.d.mcnabb@gmail.com>
2012-02-28 21:04:18 -0500

    Add devise, migration, and route."

---
+++ app/models/admin.rb
@@ -0,0 +1,6 @@
+class Admin < ActiveRecord::Base
+  devise :database_authenticatable, :trackable, :timeoutable, :validatable, :lockable
+
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :email, :password, :password_confirmation
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :admins
+
   match ':page_name' => 'pages#show'
 end
