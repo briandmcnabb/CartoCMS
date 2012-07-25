53a57d3b7cfdb249f87f9935c68593b583e05451
Brian McNabb <brian.d.mcnabb@gmail.com>
2012-07-25 14:10:30 -0400

    Add initializer file.

---
+++ lib/generators/carto_cms/install/templates/initializer.rb
@@ -0,0 +1,8 @@
+CartoCMS.setup do |config|
+
+  # ==> Dropdown Menus
+
+  #config.utility_menu_items += %w()
+  #config.cms_resource_menu_items += %w()
+  config.app_resource_menu_items += %w(shared_stories stories)
+end
